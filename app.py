d2b48de8234d9c20a9ca67f23a752a40410c7dbc
nicola <nicolatosh4@gmail.com>
2025-07-08 15:11:45 +0200

    fix app per test vercel

--- app.py
+++ app.py
@@ -14,28 +14,38 @@ You may also need to change the `listen` value in the uvicorn configuration to m
 Press Ctrl-C on the command line or send a signal to the process to stop the bot.
 """
 import asyncio
-import html
-import logging
 from dataclasses import dataclass
 from http import HTTPStatus
-
+from typing import Optional
 import uvicorn
 from asgiref.wsgi import WsgiToAsgi
+
 from flask import Flask, Response, abort, make_response, request
 
-from telegram import Update
+from const import DbType
+from links import WEBHOOK_URL
+
+import logging
+
+from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.constants import ParseMode
 from telegram.ext import (
     Application,
-    CallbackContext,
     CommandHandler,
-    ContextTypes,
+    CallbackContext,
+    CallbackQueryHandler,
+    ConversationHandler,
+    MessageHandler,
+    filters, ContextTypes,
     ExtBot,
-    TypeHandler,
+    TypeHandler
 )
 
-from credentials import BOT_TOKEN, PORT
-from links import WEBHOOK_URL
+from search import search_monologue
+
+# States for the BOT state machine
+START_ROUTES, END_ROUTES = range(2)
+MENU, MALE_MONOLOGUES, FEMALE_MONOLOGUES, CONTINUE, END = range(5)
 
 # Enable logging
 logging.basicConfig(
@@ -44,12 +54,10 @@ logging.basicConfig(
 # set higher logging level for httpx to avoid all GET and POST requests being logged
 logging.getLogger("httpx").setLevel(logging.WARNING)
 
-logger = logging.getLogger(__name__)
 
 # Define configuration constants
-
 ADMIN_CHAT_ID = 123456
-
+from credentials import BOT_TOKEN, BOT_USERNAME, PORT
 
 
 @dataclass
@@ -76,15 +84,50 @@ class CustomContext(CallbackContext[ExtBot, dict, dict, dict]):
             return cls(application=application, user_id=update.user_id)
         return super().from_update(update, application)
 
-
-async def start(update: Update, context: CustomContext) -> None:
-    """Display a message with instructions on how to use this bot."""
-    payload_url = html.escape(f"{WEBHOOK_URL}/submitpayload?user_id=<your user id>&payload=<payload>")
-    text = (
-        f"To check if the bot is still running, call <code>{WEBHOOK_URL}/healthcheck</code>.\n\n"
-        f"To post a custom update, call <code>{payload_url}</code>."
+async def start(update: Update, context: CallbackContext) -> int:
+    keyboard = [
+        [InlineKeyboardButton("Maschili", callback_data=str(MALE_MONOLOGUES))],
+        [InlineKeyboardButton("Femminili", callback_data=str(FEMALE_MONOLOGUES))],
+    ]
+    reply_markup = InlineKeyboardMarkup(keyboard)
+    await update.message.reply_text(
+        f"Unofficial RC Bot\n Ciao artista! Quale tipologia di monologhi vuoi cercare?", reply_markup=reply_markup
     )
-    await update.message.reply_html(text=text)
+    return MENU
+
+
+async def button(update: Update, context: CallbackContext) -> int:
+    query = update.callback_query
+    await query.answer()
+
+    if query.data == str(MALE_MONOLOGUES):
+        await query.edit_message_text(text="Bene! inserisci delle parole per la tua ricerca")
+        return MALE_MONOLOGUES
+    elif query.data == str(FEMALE_MONOLOGUES):
+        await query.edit_message_text(text="Bene! cerchiamo monologhi femminili")
+        return FEMALE_MONOLOGUES
+    elif query.data == str(CONTINUE):
+        await start_over(update, context)
+    elif query.data == str(END):
+        await query.edit_message_text(text=f"Ciao {query.from_user.first_name}, a presto!")
+        return ConversationHandler.END
+    else:
+        await query.edit_message_text(text="Operazione non disponibile")
+        return MENU
+
+
+async def start_over(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+    """New search for monologues"""
+    query = update.callback_query
+    keyboard = [
+        [InlineKeyboardButton("Maschili", callback_data=str(MALE_MONOLOGUES))],
+        [InlineKeyboardButton("Femminili", callback_data=str(FEMALE_MONOLOGUES))],
+    ]
+    reply_markup = InlineKeyboardMarkup(keyboard)
+    await query.edit_message_text(
+        "Bene! continuiamo la ricerca", reply_markup=reply_markup
+    )
+    return MENU
 
 
 async def webhook_update(update: WebhookUpdate, context: CustomContext) -> None:
@@ -100,6 +143,7 @@ async def webhook_update(update: WebhookUpdate, context: CustomContext) -> None:
     await context.bot.send_message(chat_id=ADMIN_CHAT_ID, text=text, parse_mode=ParseMode.HTML)
 
 
+
 async def main() -> None:
     """Set up PTB application and a web application for handling the incoming requests."""
     context_types = ContextTypes(context=CustomContext)
@@ -109,9 +153,20 @@ async def main() -> None:
         Application.builder().token(BOT_TOKEN).updater(None).context_types(context_types).build()
     )
 
-    # register handlers
-    application.add_handler(CommandHandler("start", start))
-    application.add_handler(TypeHandler(type=WebhookUpdate, callback=webhook_update))
+    # ConversationHandler to handle the state machine
+    conv_handler = ConversationHandler(
+        entry_points=[CommandHandler("start", start)],
+        states={
+            MENU: [CallbackQueryHandler(button)],
+            MALE_MONOLOGUES: [MessageHandler(filters.TEXT & ~filters.COMMAND, search_male)],
+            FEMALE_MONOLOGUES: [MessageHandler(filters.TEXT & ~filters.COMMAND, search_female)],
+            CONTINUE: [CallbackQueryHandler(start_over)],
+            END: [CallbackQueryHandler(button)]
+        },
+        fallbacks=[CommandHandler("start", start)],
+    )
+
+    application.add_handler(conv_handler)
 
     # Pass webhook settings to telegram
     await application.bot.set_webhook(url=f"{WEBHOOK_URL}/telegram", allowed_updates=Update.ALL_TYPES)
@@ -166,6 +221,57 @@ async def main() -> None:
         await application.start()
         await webserver.serve()
         await application.stop()
+
+async def search_male(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    key_list = [sub for sub in update.message.text.split()]
+    text = search(search_str=key_list, db_type=DbType.MALE)
+    await context.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text=text,
+        parse_mode=ParseMode.HTML)
+
+    keyboard = [
+        [
+            InlineKeyboardButton("Continua", callback_data=str(CONTINUE)),
+            InlineKeyboardButton("Esci", callback_data=str(END)),
+        ]
+    ]
+    reply_markup = InlineKeyboardMarkup(keyboard)
+    await update.message.reply_text("Che si fa?", reply_markup=reply_markup)
+    return MENU
+
+
+async def search_female(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    key_list = [sub for sub in update.message.text.split()]
+    text = search(search_str=key_list, db_type=DbType.FEMALE)
+    await context.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text=text,
+        parse_mode=ParseMode.HTML)
+
+    return MENU
+
+
+def search(search_str: list[str], db_type: DbType) -> Optional[str]:
+
+    # creating string
+    search_string = ""
+    for elem in search_str:
+        search_string = search_string + elem
+
+    res = search_monologue(db_type, search_string)
+    result_size = len(res)
+    if result_size:
+        if result_size > 1:
+            text = f"<b>Trovato {len(res)} monologhi:</b>\n"
+        else:
+            text = "<b>Trovato un solo monologo</b>\n"
+        for elem in res:
+            text = text + "<b>Nome:</b> " + elem["text"] + "\n"
+            text = text + "<b>Url:</b> <a href=\"" + elem["url"] + "\">" + elem["url"] + "</a>\n"
+    else:
+        text = "Mmm sembra che non ci sia nulla"
+    return text
 
 
 if __name__ == "__main__":
