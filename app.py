0283989dcc734c3029a98addff00691d2468bd78
nicola <nicolatosh4@gmail.com>
2025-07-09 00:09:33 +0200

    refactor app

--- app.py
+++ app.py
@@ -14,6 +14,7 @@ You may also need to change the `listen` value in the uvicorn configuration to m
 Press Ctrl-C on the command line or send a signal to the process to stop the bot.
 """
 import asyncio
+import json
 from dataclasses import dataclass
 from http import HTTPStatus
 from typing import Optional
@@ -38,7 +39,8 @@ from telegram.ext import (
     MessageHandler,
     filters, ContextTypes,
     ExtBot,
-    TypeHandler
+    TypeHandler,
+    Updater
 )
 
 from search import search_monologue
@@ -61,68 +63,24 @@ app = Flask(__name__)
 ADMIN_CHAT_ID = 123456
 from credentials import BOT_TOKEN, PORT
 
-@dataclass
-class WebhookUpdate:
-    """Simple dataclass to wrap a custom update type"""
 
-    user_id: int
-    payload: str
-
-
-class CustomContext(CallbackContext[ExtBot, dict, dict, dict]):
-    """
-    Custom CallbackContext class that makes `user_data` available for updates of type
-    `WebhookUpdate`.
-    """
-
-    @classmethod
-    def from_update(
-        cls,
-        update: object,
-        application: "Application",
-    ) -> "CustomContext":
-        if isinstance(update, WebhookUpdate):
-            return cls(application=application, user_id=update.user_id)
-        return super().from_update(update, application)
-
-
-"""Set up PTB application and a web application for handling the incoming requests."""
-context_types = ContextTypes(context=CustomContext)
 # Here we set updater to None because we want our custom webhook server to handle the updates
 # and hence we don't need an Updater instance
-application = (
-    Application.builder().token(BOT_TOKEN).updater(None).context_types(context_types).build()
+application= (
+    Application.builder().token(BOT_TOKEN).updater(None).build()
 )
+my_queue = asyncio.Queue()
+updater = Updater(bot=application.bot, update_queue=my_queue)
 
 
 @app.post("/telegram")  # type: ignore[misc]
 async def telegram() -> Response:
     """Handle incoming Telegram updates by putting them into the `update_queue`"""
-    logging.info("Received incoming Telegram update")
+    update = Update.de_json(data=request.json, bot=application.bot)
+    logging.info(f"Received update: {update}")
     await application.update_queue.put(Update.de_json(data=request.json, bot=application.bot))
     return Response(status=HTTPStatus.OK)
 
-
-@app.route("/submitpayload", methods=["GET", "POST"])  # type: ignore[misc]
-async def custom_updates() -> Response:
-    """
-    Handle incoming webhook updates by also putting them into the `update_queue` if
-    the required parameters were passed correctly.
-    """
-    logging.info("Received submitpayload")
-    try:
-        user_id = int(request.args["user_id"])
-        payload = request.args["payload"]
-    except KeyError:
-        abort(
-            HTTPStatus.BAD_REQUEST,
-            "Please pass both `user_id` and `payload` as query parameters.",
-        )
-    except ValueError:
-        abort(HTTPStatus.BAD_REQUEST, "The `user_id` must be a string!")
-
-    await application.update_queue.put(WebhookUpdate(user_id=user_id, payload=payload))
-    return Response(status=HTTPStatus.OK)
 
 
 @app.get("/healthcheck")  # type: ignore[misc]
@@ -143,7 +101,6 @@ async def start(update: Update, context: CallbackContext) -> int:
         f"Unofficial RC Bot\n Ciao artista! Quale tipologia di monologhi vuoi cercare?", reply_markup=reply_markup
     )
     return MENU
-
 
 async def button(update: Update, context: CallbackContext) -> int:
     query = update.callback_query
@@ -179,20 +136,6 @@ async def start_over(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
     return MENU
 
 
-async def webhook_update(update: WebhookUpdate, context: CustomContext) -> None:
-    """Handle custom updates."""
-    logging.info("Received webhook_update")
-    chat_member = await context.bot.get_chat_member(chat_id=update.user_id, user_id=update.user_id)
-    logging.info(f"New message received form {update.user_id}")
-    payloads = context.user_data.setdefault("payloads", [])
-    payloads.append(update.payload)
-    combined_payloads = "</code>\n• <code>".join(payloads)
-    text = (
-        f"The user {chat_member.user.mention_html()} has sent a new payload. "
-        f"So far they have sent the following payloads: \n\n• <code>{combined_payloads}</code>"
-    )
-    await context.bot.send_message(chat_id=ADMIN_CHAT_ID, text=text, parse_mode=ParseMode.HTML)
-
 async def main() -> None:
 
     # ConversationHandler to handle the state machine
@@ -209,20 +152,24 @@ async def main() -> None:
     )
 
     application.add_handler(conv_handler)
-    application.add_handler(TypeHandler(type=WebhookUpdate, callback=webhook_update))
 
     # Pass webhook settings to telegram
-    await application.bot.set_webhook(url=f"{WEBHOOK_URL}/telegram", allowed_updates=Update.ALL_TYPES)
+
 
     webserver = uvicorn.Server(
         config=uvicorn.Config(
             app=WsgiToAsgi(app),
             port=PORT,
             use_colors=False,
-
         )
     )
 
+    await updater.initialize()
+    await updater.start_webhook(listen="0.0.0.0",
+                                port=PORT,
+                                webhook_url=WEBHOOK_URL,
+                                allowed_updates=Update.ALL_TYPES
+                               )
     # Run application and webserver together
     async with application:
         await application.start()
